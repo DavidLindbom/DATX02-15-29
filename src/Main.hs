d8a4b8fdd4747029ea92640a9d932caee3906f55
Johan W-Schützer <johan.dss@gmail.com>
2015-02-24 19:13:42 +0100

    Fixed missmerged Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -58,7 +58,7 @@ main' args = do
         write $ "Wrote parse file to " ++ f' ++ ".parse.hpr"
 
       -- Convert to AST
-      let astE = treeE >>= collectDefs
+      let astE = treeE >>= transformModule
 
       -- Typechecker
       -- todo
