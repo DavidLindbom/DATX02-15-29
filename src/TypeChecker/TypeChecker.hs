a713f67b9ac33ecee7340dacb81971dd8638069e
Björn Norgren <bjonor@gmail.com>
2015-03-01 17:25:21 +0100

    Progress on adapting algorithm W type checker to Hopper

--- src/TypeChecker/TypeChecker.hs
+++ src/TypeChecker/TypeChecker.hs
@@ -11,13 +11,77 @@
 -- To 'go wrong' we need to add the 'recieve' construct (and others?) for which
 -- we do not know the type of the recieved data until runtime.
 -- A type system is a formal system where type checking amounts to proving the
--- type of an expression. (Inference rules, judgments)
+-- type of an expression. The proofs are carried out using inference rules over
+-- the abstract syntax tree. These rules are sometimes called judgments.
+-- (Inference rules, judgments)
 -- Based on "Algorithhm W Step by Step" by Martin Grabmüller available at
 -- https://github.com/wh5a/Algorithm-W-Step-By-Step
+-- The type checking process of algorithm W:
+-- 1. 
+
+-- Appendix I:
+-- monotype - A fully specified type (Int, [String], Int -> Int, Map Char Char)
+-- polytype - A partially applied type (a, [a] -> Int, (a -> b) -> [a] -> [b])
+-- inference rule - Tells us what type-conclusion we can draw from types from
+--                  the sub expressions.
+-- judgment - see inference rule
+-- free type variables - Type variables not bound by a quantifier.
+-- unification - A function that given two types either fails or
+--               returns a type equal to both the given types.
+-- specialization - A relation between types signifying one being more 
+--                  general and the other more "special"
+-- instantiation - "Specialization of types".
+--                 An expression of type a is instantiated to type b
+--                 if type a is more general than type b.
+-- generalization - "Generalization of types".
+--                  An expression e of type T in environment Gamma is
+--                  generalized to type (forall a.T) where a is a type
+--                  variable not free in Gamma 
 
 module TypeChecker.TypeChecker (typeCheck) where
 
+import qualified Data.Map   as Map
+import qualified Data.Set   as Set
+import qualified Data.Maybe as Maybe
+import Control.Monad.Trans.Except
+import Control.Monad.Reader
+import Control.Monad.State
+
+import qualified Text.PrettyPrint as PP
+
 import AST.AST
+
+{-
+data Module a = Mod String [Identifier] [Function a]
+
+type Identifier  = String
+type Constructor = String
+
+data Literal = LS String
+             | LC Char
+             | LI Integer
+             | LD Double
+
+data Function a = Fun Identifier a [Expression a] 
+
+type Signature = [Type]
+
+data Type = TName String [Type]
+          | TVar  String
+          | TFun  [Type] -- For functions as arguments
+
+data Pattern = PVar Identifier
+             | PCon Constructor
+             | PLit Literal
+             | PWild  
+
+data Expression a = EVar a Identifier
+                  | ECon a Constructor
+                  | ELit a Literal
+                  | ELambda a [Pattern] (Expression a)
+                  | EApp a (Expression a) (Expression a)
+                  | ECase [([Pattern], Expression a)] 
+-}
 
 typeCheck :: Module (Maybe Signature) -> Module Signature
 typeCheck = noCheck
@@ -46,4 +110,163 @@ noCheck (Mod name exported functions) = Mod name exported $ map noFun functions
       ECase   cs          -> ECase   (map (\(ps, e) -> (ps, noExp e)) cs)
       -- ECall   mt i1 i2 es -> ECall   mt i1 i2 (map noExp es)
       -- ELet    ps e1 e2    -> ELet    ps (noExp e1) (noExp e2)
-      
+
+-- Algorithm W adapted from "Algorithm W Step by Step"
+
+-- Using Exp and Lit from AST
+
+type VarName = String
+
+-- Monotype
+data TType
+  = TTVar VarName
+  | TTInt
+  | TTBool
+  | TTFun TType TType
+    deriving (Eq, Ord)
+
+-- Polytype
+data Scheme = Scheme [VarName] TType
+
+class TTypes a where
+  -- ftv collects free type variable names
+  ftv   :: a -> Set.Set VarName
+  -- apply applies a substitution where free type variables are replaced by
+  -- an instantiation(?)
+  apply :: Subst -> a -> a
+
+instance TTypes TType where
+  ftv (TTVar name)  = Set.singleton name
+  ftv (TTFun t1 t2) = ftv t1 `Set.union` ftv t2
+  ftv _             = Set.empty
+  apply s (TTVar name)  = Maybe.fromMaybe (TTVar name) (Map.lookup name s)
+  apply s (TTFun t1 t2) = TTFun (apply s t1) (apply s t2)
+  apply _ t             = t
+
+instance TTypes a => TTypes [a] where
+  ftv = foldr (Set.union . ftv) Set.empty -- why foldr instead of foldl?
+  apply s = map (apply s)
+
+instance TTypes Scheme where
+  ftv (Scheme vars t)     = ftv t Set.\\ Set.fromList vars
+  apply s (Scheme vars t) = Scheme vars (apply (foldr Map.delete s vars) t)
+
+
+
+-- Substitutions
+type Subst = Map.Map VarName TType
+
+nullSubst :: Subst
+nullSubst = Map.empty
+
+composeSubst :: Subst -> Subst -> Subst
+composeSubst s1 s2 = (Map.map (apply s1) s2) `Map.union` s1
+
+-- Type Environments
+newtype TypeEnv = TypeEnv (Map.Map String Scheme)
+
+instance TTypes TypeEnv where
+  ftv (TypeEnv e)     = ftv (Map.elems e)
+  apply s (TypeEnv e) = TypeEnv (Map.map (apply s) e)
+
+-- remove
+-- removes the binding for type x from environment e
+remove :: TypeEnv -> String -> TypeEnv
+remove (TypeEnv e) x = TypeEnv (Map.delete x e)
+
+-- generalize
+generalize :: TypeEnv -> TType -> Scheme
+generalize e t = Scheme vars t
+  where vars = Set.toList (ftv t Set.\\ ftv e)
+
+-- instantiation
+instantiate :: Scheme -> TI TType
+instantiate (Scheme vars t) = do
+  nvars <- mapM (\_ -> newTyVar "a") vars
+  let s = Map.fromList (zip vars nvars)
+  return $ apply s t
+
+
+-- "Fresh names for newly introduced type variables"
+data TIEnv = TIEnv{}
+data TIState = TIState{ tiSupply :: Int }
+
+-- ErrorT used in the tutorial is deprecated, using ExceptT instead.
+-- ExceptT gives us the 
+type TI a = ExceptT String (ReaderT TIEnv (StateT TIState IO)) a
+
+runTI :: TI a -> IO (Either String a, TIState)
+runTI t = do
+  (res, st) <- runStateT (runReaderT (runExceptT t) initTIEnv) initTIState
+  return (res, st)
+    where
+      initTIEnv = TIEnv
+      initTIState = TIState{ tiSupply = 0 }
+
+newTyVar :: String -> TI TType
+newTyVar prefix = do
+  s <- get
+  put s{ tiSupply = tiSupply s + 1 }
+  return (TTVar (prefix ++ show (tiSupply s)))
+
+-- Unification
+mgu :: TType -> TType -> TI Subst
+mgu (TTFun t1 t2) (TTFun t3 t4) = do
+                                s1 <- mgu t1 t3
+                                s2 <- mgu (apply s1 t2) (apply s1 t4)
+                                return (s1 `composeSubst` s2)
+mgu (TTVar s) t = varBind s t
+mgu t (TTVar s) = varBind s t
+mgu TTInt TTInt = return nullSubst
+mgu TTBool TTBool = return nullSubst
+mgu t1 t2 = throwE $ "types do not unify: " ++ show t1 ++ " vs. " ++
+              show t2
+
+varBind :: VarName -> TType -> TI Subst
+varBind s t | t == (TTVar s) = return nullSubst
+             | s `Set.member` ftv t = throwE $ "occurs check fails: " ++
+               s ++ " vs. " ++ show t
+             | otherwise = return (Map.singleton s t)
+
+-- Main type inference function
+
+tiLit :: Literal -> TI (Subst, TType)
+tiLit = undefined
+
+ti :: TypeEnv -> Expression -> TI (Subst, Type) -- Do we need a in expr?
+ti (TypeEnv env) (EVar n)       = case 
+ti _             (Elit l)       = undefined
+ti env           (EAbs n e)     = undefined
+ti env       exp@(EApp e1 e2)   = undefined
+ti env           (ELet x e1 e2) = undefined
+
+typeInference :: Map.Map String Scheme -> Expression -> TI TType
+typeInference env e = do
+  (s, t) <- ti (TypeEnv env) e
+  return (apply s t)
+
+-- Pretty printer
+
+instance Show TType where
+  showsPrec _ t = shows (prType t)
+    where
+      prType :: TType -> PP.Doc
+      prType (TTVar name)  = PP.text name
+      prType TTInt         = PP.text "Int"
+      prType TTBool        = PP.text "Bool"
+      prType (TTFun t1 t2) = prParenType t1 PP.<+> PP.text "->" PP.<+> prType t2
+      prParenType :: TType -> PP.Doc
+      prParenType t = case t of
+        TTFun _ _ -> PP.parens (prType t)
+        _         -> prType t
+
+instance Show Scheme where
+  showsPrec _ s = shows (prScheme s)
+    where
+      prScheme :: Scheme -> PP.Doc
+      prScheme (Scheme vars t) =
+        PP.text "forall"
+        PP.<+> PP.hcat (PP.punctuate PP.comma (map PP.text vars))
+        PP.<> PP.text "." PP.<+> PP.text (show t)
+
+
