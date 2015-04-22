22e6b4844310d8fb225e3c93846629d1689e9eca
Johan W-Schützer <johan.dss@gmail.com>
2015-04-22 16:07:39 +0200

    First preparation for apps in codegen

--- src/CodeGenerator/CodeGenerator.hs
+++ src/CodeGenerator/CodeGenerator.hs
@@ -24,6 +24,9 @@ import Utils.ErrM
 import AST.AST as HPR
 
 import Data.Char
+import qualified Data.Set as S
+import Data.List (break, intercalate)
+import qualified Control.Monad.Reader as R
 
 -- |The 'compileModule' function compiles a hopper Module
 --  to a CoreErlang<F6>fe<F6>tax.Module
@@ -55,8 +58,8 @@ compileExport m eId = CES.Function (Atom eId, getArity eId m)
 -- |The 'compileFun' function compiles a hopper Function
 --  to a CoreErlang.FunDef
 compileFun :: HPR.Function Type -> FunDef
-compileFun (HPR.Fun fId t e) = 
-  CES.FunDef (Constr (Function (Atom fId, typeToArity t))) (Constr (compileExp e'))
+compileFun (HPR.Fun fId t e) =
+  CES.FunDef (Constr (Function (Atom fId, typeToArity t))) (Constr (R.runReader (compileExp e') S.empty))
   where e' = case e of
                (ELambda _ _) -> e
                _             -> (ELambda [] e)
@@ -66,19 +69,61 @@ compileFun (HPR.Fun fId t e) =
 --  Named and AppAst are implemented with parameterless functions in mind
 --  AppAST will rely on that the first AST in the first occurence of
 --  an AppAST will be a function identifier
-compileExp :: Expression -> CES.Exp
-compileExp (EVar nId)         = Var $ nId
-compileExp (ELit l)           = Lit $ compileLiteral l
-compileExp (ETuple es)        = Tuple $ map (\e -> Exp (Constr (compileExp e))) es
+compileExp :: Expression -> R.Reader (S.Set Identifier) CES.Exp
+compileExp (EVar nId)         = case strToName nId of
+                                  (Nothing, _) -> do isVariable <- R.asks $ S.member nId
+                                                     return $ if isVariable
+                                                                 then Var nId
+                                                                 else CES.Fun $ __fun nId
+                                  (Just mId, fId) -> return $ modCall 
+                                                     (atom "erlang")
+                                                     (atom "make_fun")
+                                                     [atom $ intercalate "." mId,
+                                                     atom $ "__"++fId,
+                                                     Lit $ LInt 0]
+compileExp (ELit l)           = return $ Lit $ compileLiteral l
+compileExp (ETuple es)        = fmap Tuple $ mapM (\e -> fmap exps $ compileExp e) es
 compileExp l@(ELambda pats e) = compileLambda l
-compileExp (EApp ef ex)       = App (Exp (Constr (compileExp ef))) [Exp (Constr (compileExp ex))]
-compileExp (ECase e cases)    = Case (Exp (Constr (compileExp e))) (compileCases cases)
-compileExp (ECall mId fId e)  = ModCall (m, fun) (map f as)
+compileExp (EApp ef ex)       = do ef' <- compileExp ef
+                                   ex' <- compileExp ex
+                                   return $ App (exps ef') [exps ex']
+compileExp (ECase e cases)    = do exp <- compileExp e
+                                   alts <- mapM (\(pat,res)->
+                                                 do 
+                                                   let cpat = compileCasePat pat
+                                                       vars = namesOccP pat
+                                                   cres <- R.local 
+                                                           (inserts vars) $
+                                                        compileExp res
+                                                   return $ Constr $ Alt
+                                                       (pats cpat)
+                                                       (Guard$
+                                                        exps$
+                                                        atom"true")
+                                                       (exps cres)) cases
+                                   return $ Case (exps exp) alts
+compileExp (ECall mId fId e)  = fmap (ModCall (m, fun)) (mapM (fmap exps . compileExp) as)
   where m           = Exp (Constr (Lit (LAtom (Atom mId))))
         fun         = Exp (Constr (Lit (LAtom (Atom fId))))
         (ETuple as) = e
-        f x = Exp (Constr (compileExp x))
 compileExp e                  = error $ "Illegal expression: " ++ show e
+
+pats :: Pat -> Pats
+pats = Pat
+
+exps :: Exp -> Exps
+exps = Exp . Constr
+
+atom = Lit . LAtom . Atom 
+
+namesOccP pat = case pat of
+                  HPR.PVar pId    -> [pId]
+                  HPR.PCon _ pats -> pats >>= namesOccP
+                  HPR.PTuple pats -> pats >>= namesOccP
+                  _               -> []
+
+-- TODO WRITE TYPE SIG
+inserts ns set = foldr (\n set -> S.insert n set) set ns
 
 -- |The 'compileLambda' function converts a
 --  hopper lambda expression to a core erlang lambda.
@@ -87,28 +132,17 @@ compileExp e                  = error $ "Illegal expression: " ++ show e
 -- have a list of variables as the pattern. Whenever the hopper
 -- pattern contains something else, it should replace it with a
 -- fresh variable and wrap the expression with a case clause
-compileLambda :: Expression -> CES.Exp
-compileLambda (ELambda pats e) = Lambda vars (Exp (Constr exp))
-  where exp = Case caseExps caseAlts
-        caseAlts = [Constr $ Alt casePats (Guard (Exps (Constr []))) (Exp (Constr $ compileExp e))]
-        casePats = Pats $ map compileCasePat pats
-        caseExps = Exps (Constr [Constr (Var x) | x <- vars])
-        vars = newVars (length pats)
-        newVars 0 = ["X@0"]
-        newVars x = ("X@" ++ show x) : newVars (x-1)
-compileLambda e                = error $ "Not a lambda: " ++ show e
+compileLambda :: Expression -> R.Reader (S.Set Identifier) CES.Exp
+compileLambda l@(ELambda pats e) = compileExp $ lambdaToCase l
+compileLambda e                  = error $ "Not a lambda: " ++ show e
 
--- |The 'compileCases' function converts a list of
---  cases to a list of annotated alts as seen in
---  Language.CoreErlang.Syntax case expressions
-compileCases :: [(Pattern, Expression)] -> [Ann Alt]
-compileCases [] = []
-compileCases ((p, e):rest) = Constr (Alt pats guard exps ) : compileCases rest
-  where pats  = Pat $ compileCasePat p
-        guard = Guard (Exp (Constr (Lit (LAtom (Atom "true"))))) -- Change when guards are implemented
-        exps  = Exp (Constr (compileExp e))
+-- \a (b,c) -> a + b
+-- \X@1 X@0 -> case (X@1, X@0) of
+--               (a, (b,c)) -> a + b
+lambdaToCase :: Expression -> Expression
+lambdaToCase (ELambda pats e) = let vns = map (("X@"++) . show) (zipWith const [1..] pats) in
+       ELambda (map HPR.PVar vns) $ ECase (HPR.ETuple $ map EVar vns) [(HPR.PTuple pats, e)]
 
--- |The 'getCasePatterns' function converts a pattern
 --  to a list of patterns. This means a tuple will be
 --  converted to a list of its patterns.
 getCasePatterns :: Pattern -> [Pattern]
@@ -166,6 +200,21 @@ getTypeSig fId (Mod mId es (HPR.Fun funId typeSig _:defs) datas)
 --  signatures from the give module
 getSignatures :: HPR.Module Type -> [(Identifier, Integer)]
 getSignatures (Mod _ _ defs _) = map (\(HPR.Fun i sig _) -> (i, 1)) defs
+
+__fun :: Identifier -> CES.Function
+__fun s = Function (Atom$"__"++s,0)
+
+modCall :: Exp -> Exp -> [Exp] -> Exp
+modCall mod fn args = ModCall (exps mod,exps fn) $ map exps args
+
+strToName :: Identifier -> (Maybe [String], String)
+strToName (c:s) | isUpper c = case break (=='.') (c:s) of
+                                (con,"") -> (Nothing, con)
+                                (mod,'.':s') -> addModule mod $ strToName s'
+                | otherwise = (Nothing, c:s )
+                where
+                  addModule m (Nothing, s) = (Just[m], s)
+                  addModule m (Just ms, s) = (Just$m:ms, s)
 
 -- |The 'generateModuleInfo' function generates a list of
 --  CoreErlang.FunDec of containing the module_info/0 and
